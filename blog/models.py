8a001197d316b78f56aa1eed01645cd542307050
Hu Tiger <huxy99@yahoo.com>
2020-07-22 18:14:43 -0500

    add models: Category, Tag, Post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, null=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, null=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now_add=True)
+    abstract = models.CharField(max_length=200, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True, related_name="tags")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+if __name__ == "__main__":
+    pass
