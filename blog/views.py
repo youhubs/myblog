b24574b695514b0cbf84fdf44a0c17fa5c47dde8
Hu Tiger <huxy99@yahoo.com>
2020-07-23 09:36:02 -0500

    post detail veiw and template

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Post
 
@@ -7,3 +7,12 @@ def index(request):
     posts = Post.objects.all().order_by('-created_at')
     context = {'posts': posts}
     return render(request, 'blog/index.html', context)
+
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/detail.html', context={'post': post})
+
+
+if __name__ == "__main__":
+    pass
