7eadc1ba13c20ce3e2b1b3e378060f981c44af0d
Hu Tiger <huxy99@yahoo.com>
2020-07-22 22:39:42 -0500

    update admin: display and save

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Category, Tag, Post
 
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_at', 'modified_at', 'category', 'author']
+    fields = ['title', 'content', 'abstract', 'category', 'tags']
+
+    def save_model(self, request, obj, form, change):
+        obj.author = request.user
+        super().save_model(request, obj, form, change)
+
+
 admin.site.register(Category)
 admin.site.register(Tag)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
