e79435c6bdc8ab927cbe1448a6e5527411b19006
Hu Tiger <huxy99@yahoo.com>
2020-07-23 22:42:32 -0500

    add class views from function views

--- blog/views.py
+++ blog/views.py
@@ -1,50 +1,62 @@
 import markdown
 import re
-
 from markdown.extensions.toc import TocExtension
+
 from django.utils.text import slugify
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from .models import Post, Category, Tag
 
 
-def index(request):
-    posts = Post.objects.all()
-    context = {'posts': posts}
-    return render(request, 'blog/index.html', context)
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
 
 
-def detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    post.increase_views()
-    md = markdown.Markdown(extensions=[
-        'markdown.extensions.extra',
-        'markdown.extensions.codehilite',
-        TocExtension(slugify=slugify),
-    ])
-    post.content = md.convert(post.content)
-
-    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
-    post.toc = m.group(1) if m is not None else ''
-    return render(request, 'blog/detail.html', context={'post': post})
+class ArchiveView(IndexView):
+    def get_queryset(self):
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        return super().get_queryset().filter(created_at__year=year, created_at__month=month)
 
 
-def archive(request, year, month):
-    posts = Post.objects.filter(
-        created_at__year=year, created_at__month=month)
-    return render(request, 'blog/index.html', context={'posts': posts})
+class CategoryView(IndexView):
+    def get_queryset(self):
+        cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+        return super(CategoryView, self).get_queryset().filter(category=cate)
 
 
-def category(request, pk):
-    cate = get_object_or_404(Category, pk=pk)
-    posts = Post.objects.filter(category=cate)
-    return render(request, 'blog/index.html', context={'posts': posts})
+class TagView(IndexView):
+    def get_queryset(self):
+        tag = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
+        return super(TagView, self).get_queryset().filter(tags=tag)
 
 
-def tag(reques, pk):
-    tag = get_object_or_404(Tag, pk=pk)
-    posts = Post.objects.filter(tags=tag)
-    return render(reques, 'blog/index.html', context={'posts': posts})
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+
+    def get(self, request, *args, **kwargs):
+        response = super(PostDetailView, self).get(request, *args, **kwargs)
+        self.object.increase_views()
+        return response
+
+    def get_object(self, queryset=None):
+        post = super().get_object(queryset=None)
+        md = markdown.Markdown(extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+            TocExtension(slugify=slugify),
+        ])
+        post.content = md.convert(post.content)
+
+        m = re.search(
+            r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+        post.toc = m.group(1) if m is not None else ''
+        return post
 
 
 if __name__ == "__main__":
